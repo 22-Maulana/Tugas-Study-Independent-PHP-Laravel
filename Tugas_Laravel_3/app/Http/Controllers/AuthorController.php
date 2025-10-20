6a7799df0e30d2665fe4abd819e4b47ff033ff45
Maulana Ardhiansyah Prasetyo <143920987+22-Maulana@users.noreply.github.com>
2025-10-20 11:26:33 +0700

    tes

--- Tugas_Laravel_3/app/Http/Controllers/AuthorController.php
+++ Tugas_Laravel_3/app/Http/Controllers/AuthorController.php
@@ -2,23 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Author; // Pastikan model Author sudah ada
+use App\Models\Author;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 
 class AuthorController extends Controller
 {
     /**
-     * Menampilkan daftar semua author.
      *
      * @return \Illuminate\Http\JsonResponse
      */
     public function index(): JsonResponse
     {
-        // Mengambil semua data author dari database
         $authors = Author::all();
 
-        // Mengembalikan data dalam format JSON
         return response()->json([
             'success' => true,
             'message' => 'Daftar Author Berhasil Diambil',
