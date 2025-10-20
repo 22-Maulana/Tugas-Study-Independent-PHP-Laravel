242f36ff01b233a2c6c98e5a602a8f33e31a7a1a
Maulana Ardhiansyah Prasetyo <143920987+22-Maulana@users.noreply.github.com>
2025-10-20 11:26:53 +0700

    tes2
    Refactor BookController to improve code clarity.

--- Tugas_Laravel_3/app/Http/Controllers/BookController.php
+++ Tugas_Laravel_3/app/Http/Controllers/BookController.php
@@ -2,23 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Book; // Pastikan model Book sudah ada
+use App\Models\Book; 
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 
 class BookController extends Controller
 {
     /**
-     * Menampilkan daftar semua buku.
      *
      * @return \Illuminate\Http\JsonResponse
      */
     public function index(): JsonResponse
     {
-        // Mengambil semua data buku dari database
         $books = Book::all();
 
-        // Mengembalikan data dalam format JSON
         return response()->json([
             'success' => true,
             'message' => 'Daftar Buku Berhasil Diambil',
